bba9ac7cb6d5c5e1225388160123606b36064e4c
Darshan-Bajeja <codingdsb@gmail.com>
2021-05-29 09:08:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function getNewJoke() {
+    
+    
+    fetch('https://v2.jokeapi.dev/joke/Any?type=twopart')
+        .then(response => response.json())
+        .then(data => {
+            setup = data.setup
+            delivery = data.delivery
+            document.querySelector('.part1').innerHTML = setup 
+            emojis = [
+                '😅', '😁', '😂', '😆', '🤣', '😹', '😜'
+            ]
+            randomIndexForEmoji = Math.floor(Math.random() * 7)
+            document.querySelector('.part2').innerHTML = delivery + " " + emojis[randomIndexForEmoji] + emojis[randomIndexForEmoji]  + emojis[randomIndexForEmoji] 
+
+        })
+    
+}
+
+document.getElementById("reload").addEventListener("click", getNewJoke);
+
+window.onload = getNewJoke()
